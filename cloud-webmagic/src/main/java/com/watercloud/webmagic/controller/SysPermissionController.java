63daa27e89f82aab18ec5ac4b45084604f25b504
lly <1518189682@qq.com>
2022-04-22 22:59:33 +0800

    菜单接口静态数据

--- cloud-webmagic/src/main/java/com/watercloud/webmagic/controller/SysPermissionController.java
+++ cloud-webmagic/src/main/java/com/watercloud/webmagic/controller/SysPermissionController.java
@@ -1,6 +1,12 @@
 package com.watercloud.webmagic.controller;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.cloudwater.common.commonVo.Result;
+import com.watercloud.webmagic.common.util.CommonConstant;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -17,4 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sys-permission")
 public class SysPermissionController {
 
+    @PostMapping("/getRouters")
+    public Result<JSONArray> getRouters(){
+        Result<JSONArray> result = Result.OK();
+        String str = "[{\"path\":\"/icon\",\"component\":\"Layout\",\"children\":[{\"path\":\"index\",\"component\":\"icons/index\",\"name\":\"Icons\",\"meta\":{\"title\":\"icons\",\"icon\":\"icon\",\"noCache\":true}}]}]";
+        JSONArray res = (JSONArray) JSON.parse(str);
+        result.setCode(CommonConstant.SC_OK_200);
+        result.setData(res);
+        return result;
+    }
 }
