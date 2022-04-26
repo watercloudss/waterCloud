4f5d6a612b723df8537b0f4b538b4cf7694fca80
lly <1518189682@qq.com>
2022-04-26 23:09:58 +0800

    跨域配置修改

--- cloud-webmagic/src/main/java/com/watercloud/webmagic/config/CorsConfig.java
+++ cloud-webmagic/src/main/java/com/watercloud/webmagic/config/CorsConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.filter.CorsFilter;
  * 跨域配置
  */
 
-@Configuration
+//@Configuration
 public class CorsConfig {
     @Bean
     public FilterRegistrationBean corsFilter() {
